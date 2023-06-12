ea7a6b7b0c3c3aeeb6daec2b73885b2e28fc6243
Jasmine <jasmine@Jasmines-MBP.attlocal.net>
2023-06-12 16:01:11 -0500

    testing initial cart function

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,4 +1,7 @@
-/* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+/* Create an array named products which 
+you will use to add all of your product object literals 
+that you create in the next step. */
+let products = []
 
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
@@ -15,25 +18,99 @@
    - strawberry.jpg by Allec Gomes
 */
 
-/* Declare an empty array named cart to hold the items in the cart */
+let product1 = {
+  name:"Carton of Cherries",
+  price:4.00,
+  quantity:0,
+  productId:1,
+  image: "src/images/cherry.jpg"
+};
 
+let product2 = {
+  name: "Carton of Strawberries",
+  price: 5.00,
+  quantity:0,
+  productId:2,
+  image: "src/images/strawberry.jpg"
+};
+
+let product3 = {
+  name: "Bag of Oranges",
+  price:10.00,
+  quantity:0,
+  productId:3,
+  image: "src/images/orange.jpg"
+};
+products.push(product1,product2,product3)
+/* Declare an empty array named cart to hold the items in the cart */
+let cart = []
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
+function addProductToCart(productId) {
+//addProductToCart should get the correct product based on the productId 
+  let curProduct = products.find(product => product.productId === productId);
+  
+  if (curProduct){
+    const cartItem = cart.find(item => item.productId === productId);
+    if (cartItem){
+      cartItem.quantity++;
+    }else{
+      cart.push({ ...curProduct, quantity: 1 }); 
+  }
+}
+}
 
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
+function increaseQuantity(productId) {
+  // Find the product based on the productId
+  const cartItem = cart.find(item => item.productId === productId);
+
+  // If product exists, increase its quantity
+  if (cartItem) {
+    cartItem.quantity++;
+    console.log(`Quantity of ${cartItem.name} increased to ${cartItem.quantity}`);
+  } else {
+    console.log('cartItem not found');
+  }
+}
 
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
+function decreaseQuantity(productId){
+  
+  const cartItem = cart.find(item => item.productId === productId);
+  console.log(cartItem)
+  const cartItemIndex = cart.findIndex(product => product.productId === productId);
 
+  // // If product exists, decrease its quantity
+  // if (productIndex !== -1) {
+  //   const product = products[productIndex];
+
+  //   if (product.quantity > 0) {
+  //     product.quantity--;
+
+  //     if (product.quantity === 0) {
+  //       products.splice(productIndex, 1);
+  //       console.log(`Product ${product.name} removed from the cart`);
+  //     } else {
+  //       console.log(`Quantity of ${product.name} decreased to ${product.quantity}`);
+  //     }
+  //   } else {
+  //     console.log(`Product ${product.name} is already out of stock`);
+  //   }
+  // } else {
+  //   console.log('Product not found');
+  // }
+}
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
@@ -66,11 +143,11 @@ module.exports = {
    cart,
    addProductToCart,
    increaseQuantity,
-   decreaseQuantity,
-   removeProductFromCart,
-   cartTotal,
-   pay, 
-   emptyCart,
+  decreaseQuantity}
+  //  removeProductFromCart,
+  //  cartTotal,
+  //  pay, 
+  //  emptyCart,
    /* Uncomment the following line if completing the currency converter bonus */
-   // currency
-}
+   // currencop
+// }
